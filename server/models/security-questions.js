be8283bf2ac1f405a2251f39e7e3e79d1eeb21c0
JohnMVan <john.vanhessche@gmail.com>
2023-04-18 18:58:19 -0500

    security-questions.js file - jv

--- server/models/security-questions.js
+++ server/models/security-questions.js
@@ -1,10 +1,20 @@
 /*
 =====================================================
-; File Name:
+; File Name:  security-question.js
 ; Project: Gryffindor - Bob's Computer Repair Shop
-; Author:
-; Date:
-; File Description:
+; Author: John Vanhessche
+; Date: 18 April 2023
+; File Description:  Model for security questions
 ; Modifications:
 =====================================================
 */
+
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+let securityQuestionSchema = new Schema({
+    text: { type: String },
+    isDisabled: { type: Boolean, default: false }
+}, { collection: 'securityQuestions' })
+
+module.exports = mongoose.model('SecurityQuestion', securityQuestionSchema);
