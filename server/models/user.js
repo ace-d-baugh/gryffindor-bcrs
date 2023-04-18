b97a5c80da1e6691dc176cdfd071b943d6478411
Ace Baugh <ace.d.baugh@gmail.com>
2023-04-18 10:43:44 -0400

    user API created

--- server/models/user.js
+++ server/models/user.js
@@ -1,10 +1,38 @@
 /*
 =====================================================
-; File Name:
+; File Name: user.js
 ; Project: Gryffindor - Bob's Computer Repair Shop
-; Author:
-; Date:
-; File Description:
+; Author: Ace Baugh
+; Date: 04/18/2023
+; File Description: This file creates the user model
 ; Modifications:
 =====================================================
 */
+
+//require statements
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const UserRoleSchema = require("../schemas/user-role");
+const SelectedSecurityQuestionsSchema = require("../schemas/selected-security-questions");
+
+//user schema
+let UserSchema = new Schema(
+  {
+    username: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    firstName: { type: String },
+    lastName: { type: String },
+    phoneNumber: { type: String },
+    address: { type: String },
+    email: { type: String },
+    isDisabled: { type: Boolean, default: false },
+    role: UserRoleSchema,
+    selectedSecurityQuestions: [SelectedSecurityQuestionsSchema],
+    dateCreated: { type: Date, default: new Date() },
+    dateModified: { type: Date },
+  },
+  { collection: "users" }
+);
+
+//export the model
+module.exports = mongoose.model("User", UserSchema);
