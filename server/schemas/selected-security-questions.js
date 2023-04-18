a5b5339c0e2f80f5305f6aa852ce7738951b08ad
Ace Baugh <ace.d.baugh@gmail.com>
2023-04-18 10:47:59 -0400

    Added SelectedSecurityQuestionsSchema

--- server/schemas/selected-security-questions.js
+++ server/schemas/selected-security-questions.js
@@ -1,10 +1,23 @@
 /*
 =====================================================
-; File Name:
+; File Name: selected-security-questions.js
 ; Project: Gryffindor - Bob's Computer Repair Shop
-; Author:
-; Date:
-; File Description:
+; Author: Ace Baugh
+; Date: 04/18/2023
+; File Description: This file defines the selected security questions schema
 ; Modifications:
 =====================================================
 */
+
+//require statements
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+//selected security questions schema
+let SelectedSecurityQuestionsSchema = new Schema({
+  questionText: { type: String },
+  answerText: { type: String },
+});
+
+//export the model
+module.exports = SelectedSecurityQuestionsSchema;
