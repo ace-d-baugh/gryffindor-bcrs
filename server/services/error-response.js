ec5f80f9f10cd9f3e1dddaed198dca7c2f16a9c0
Ace Baugh <ace.d.baugh@gmail.com>
2023-04-18 21:06:00 -0400

    error-response done

--- server/services/error-response.js
+++ server/services/error-response.js
@@ -8,3 +8,21 @@
 ; Modifications:
 =====================================================
 */
+
+class ErrorResponse {
+  constructor(httpCode, message, data) {
+    this.httpCode = httpCode;
+    this.message = message;
+    this.data = data;
+  }
+
+  toObject() {
+    return {
+      httpCode: this.httpCode,
+      message: this.message,
+      data: this.data,
+    };
+  }
+}
+
+module.exports = ErrorResponse;
