910f723e8249321983c631a55efdfd53886c5a2a
Chad ONeal <chadoneal3@gmail.com>
2023-04-18 21:29:00 -0400

    day-27

--- server/schemas/user-role.js
+++ server/schemas/user-role.js
@@ -1,11 +1,19 @@
 /*
 =====================================================
-; File Name:
+; File Name: user-role.js
 ; Project: Gryffindor - Bob's Computer Repair Shop
-; Author:
-; Date:
-; File Description:
-; Modifications:
+; Author: Richard Krasso
+: modified by: Chad ONeal
+; Date: 04/18/2023
 =====================================================
 */
 
+// require statements
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+let userRoleSchema = new Schema({
+    text: {type: String, default: 'standard'}
+});
+
+module.exports = userRoleSchema;
