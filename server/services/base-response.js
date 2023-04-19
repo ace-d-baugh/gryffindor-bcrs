7fd299f309b2d3ee165e1aaa9327d5c85ce4c7d1
Ace Baugh <ace.d.baugh@gmail.com>
2023-04-18 21:02:56 -0400

    base response

--- server/services/base-response.js
+++ server/services/base-response.js
@@ -8,3 +8,19 @@
 ; Modifications:
 =====================================================
 */
+
+class BaseResponse {
+  constructor(httpCode, message, data) {
+    this.httpCode = httpCode;
+    this.message = message;
+    this.data = data;
+  }
+
+  toObject() {
+    return {
+      httpCode: this.httpCode,
+      message: this.message,
+      data: this.data,
+    };
+  }
+}
