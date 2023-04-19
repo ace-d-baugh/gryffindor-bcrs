664274d9d85a4c0955d1f506aee4f32c2102c9ff
Ace Baugh <ace.d.baugh@gmail.com>
2023-04-18 23:16:44 -0400

    More Attribution

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,3 +1,14 @@
+/*
+=====================================================
+; File Name: app-routing.module.ts
+; Project: Gryffindor - Bob's Computer Repair Shop
+; Author: Richard Krasso
+; Date: 04/18/2023
+; File Description: App routing module
+; Modifications: Ace Baugh
+=====================================================
+*/
+
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { BaseLayoutComponent } from './shared/base-layout/base-layout.component';
