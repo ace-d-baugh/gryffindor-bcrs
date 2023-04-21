fbf07eb3a905eba253cad9aebf4df8f0febf4ad4
JohnMVan <john.vanhessche@gmail.com>
2023-04-20 19:53:17 -0500

    user.service.ts and user.interface.ts - JMV

---
+++ src/app/shared/services/user.service.ts
@@ -0,0 +1,58 @@
+/*
+=====================================================
+; File Name: user.service.ts
+; Project: Gryffindor - Bob's Computer Repair Shop
+; Author: Richard Krasso
+; Date: 04/20/2023
+; File Description: user.service.ts
+; Modifications: John Vanhessche
+=====================================================
+*/
+
+import { Injectable } from "@angular/core";
+import { User } from "../models/user.interface";
+import { HttpClient } from "@angular/common/http";
+import { Observable } from 'rxjs';
+
+@Injectable({
+    providedIn: 'root'
+})
+
+export class UserService {
+    
+    constructor(private http: HttpClient) {}
+
+    findAllUsers():Observable<any> {
+        return this.http.get('/api/users');
+    }
+
+    findUserById(userId: string): Observable<any> {
+        return this.http.get('/api/users' + userId);
+    }
+
+    createUser(user: User): Observable<any> {
+        return this.http.post('/api/users', {
+            username: user.username,
+            password: user.password,
+            firstname: user.firstname,
+            lastname: user.lastname,
+            phonenumber: user.phonenumber,
+            address: user.address,
+            email: user.email
+        })
+    }
+
+    updateUser(userId: string, user: User): Observable<any> {
+        return this.http.put('/api/users/' + userId, {
+            firstname: user.firstname,
+            lastname: user.lastname,
+            phonenumber: user.phonenumber,
+            address: user.address,
+            email: user.email
+        })
+    }
+
+    deleteUser(userId: string): Observable<any> {
+        return this.http.delete('/api/users' + userId);
+    }
+}
