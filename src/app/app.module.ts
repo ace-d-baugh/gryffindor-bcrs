7d54e38bce2709bb47f10f3f935a8e06311cafa0
JohnMVan <john.vanhessche@gmail.com>
2023-04-20 19:24:26 -0500

    update app.module.ts & package.json- JMV

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,14 +15,31 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HomeComponent } from './pages/home/home.component';
-import { AuthLayoutComponent } from './shared/auth-layout/auth-layout.component';
-import { BaseLayoutComponent } from './shared/base-layout/base-layout.component';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatTableModule } from '@angular/material/table';
+import { MessageModule } from 'primeng/message';
+import { MessagesModule } from 'primeng/messages';
+import { TableModule } from 'primeng/table';
+import { ButtonModule } from 'primeng/button';
+import { ConfirmDialogModule } from 'primeng/confirmdialog';
+import { ConfirmationService } from 'primeng/api';
+
+//component imports
+import { HomeComponent } from './pages/home/home.component';
+import { AuthLayoutComponent } from './shared/auth-layout/auth-layout.component';
+import { BaseLayoutComponent } from './shared/base-layout/base-layout.component';
 import { LandingComponent } from './pages/landing/landing.component';
+import { UserListComponent } from './pages/user-list/user-list.component';
 
 @NgModule({
   declarations: [
@@ -31,6 +48,7 @@ import { LandingComponent } from './pages/landing/landing.component';
     LandingComponent,
     AuthLayoutComponent,
     BaseLayoutComponent,
+    UserListComponent,
   ],
   imports: [
     BrowserModule,
@@ -40,6 +58,20 @@ import { LandingComponent } from './pages/landing/landing.component';
     MatToolbarModule,
     MatButtonModule,
     MatIconModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatMenuModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatDialogModule,
+    MatTableModule,
+    MessageModule,
+    MessagesModule,
+    TableModule,
+    ButtonModule,
+    ConfirmDialogModule
   ],
   providers: [],
   bootstrap: [AppComponent],
