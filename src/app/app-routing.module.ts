de9d88254fa7e27d02fcd569fe210f9bdd4aa792
Chad ONeal <chadoneal3@gmail.com>
2023-04-21 20:53:17 -0400

    signin page, not complete, links to home page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -35,7 +35,7 @@ const routes: Routes = [
     },
     {
       path: 'session',
-      component: AuthLayoutComponent,
+      component: BaseLayoutComponent,
       children: [
         {
           path: 'sign-in',
--- src/app/shared/session.service.ts
+++ src/app/shared/session.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { User } from '../shared/models/user.interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SessionService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  signIn(username: string, password: string): Observable<any> {
+    return this.http.post('/api/session/login', {
+      username,
+      password
+    })
+  }
 }
