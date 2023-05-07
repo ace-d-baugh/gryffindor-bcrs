b27190230abe87793628ba26c9640cd2af2b1e7b
Chad ONeal <chadoneal3@gmail.com>
2023-05-07 19:31:45 -0400

    gave user-list delete dialog

--- src/app/pages/user-list/user-list.component.ts
+++ src/app/pages/user-list/user-list.component.ts
@@ -1,4 +1,4 @@
-/*
+ /*
 =====================================================
 ; File Name: user-list.component.ts
 ; Project: Gryffindor - Bob's Computer Repair Shop
@@ -9,68 +9,90 @@
 =====================================================
 */
 
-//import statements
 import { Component, OnInit } from '@angular/core';
-import { User } from '../../shared/models/user.interface';
-import { UserService } from '../../shared/services/user.service';
-import { ConfirmationService, ConfirmEventType } from 'primeng/api';
+import { User } from 'src/app/shared/models/user.interface';
+import { UserService } from 'src/app/shared/services/user.service';
+import { ConfirmationService, ConfirmEventType, MessageService } from 'primeng/api';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Message } from 'primeng/api';
 
-//export class
 @Component({
   selector: 'app-user-list',
   templateUrl: './user-list.component.html',
   styleUrls: ['./user-list.component.css'],
+  providers: [ConfirmationService, MessageService]
 })
-
-//export class
 export class UserListComponent implements OnInit {
-  users: User[] = [];
 
-  //  constructor
-  constructor(
-    private userService: UserService,
-    private confirmationService: ConfirmationService
-  ) {
-    this.userService.findAllUsers().subscribe({
-      next: (res) => {
-        this.users = res.data;
-      },
-      error: (e) => {
-        console.log(e);
-      },
-    });
+  users: User[];
+  errorMessages: Message[];
+
+  userForm: FormGroup = this.fb.group({
+    username: [null, Validators.compose([Validators.required])],
+  });
+
+  constructor(private userService: UserService, private confirmationService: ConfirmationService, private fb: FormBuilder, private messageService: MessageService)
+  {
+    this.users = [];
+    this.errorMessages = [];
+
+    this.userService.findAllUsers().subscribe
+    (
+      {
+        next: (res) =>
+        {
+          this.users = res.data;
+        },
+        error: (e) =>
+        {
+          console.log(e);
+        }
+      }
+    )
   }
 
-  //ngOnInit
-  ngOnInit(): void {}
+  ngOnInit(): void {
+  }
 
-  //delete function
-  delete(userId: string): void {
-    this.confirmationService.confirm({
-      message: 'Are you sure that you want to delete this record?',
-      header: 'Confirmation',
-      icon: 'pi pi-exclamation-triangle',
-      accept: () => {
-        this.userService.deleteUser(userId).subscribe({
-          next: (res) => {
-            console.log('User deleted successfully');
-            this.users = this.users.filter((user) => user._id !== userId);
-          },
-          error: (e) => {
-            console.log(e);
-          },
-        });
-      },
-      reject: (type: any) => {
-        switch (type) {
-          case ConfirmEventType.REJECT:
-            console.log('User rejected this operation');
-            break;
-          case ConfirmEventType.CANCEL:
-            console.log('User cancelled this operation');
-            break;
+  delete(userId: string)
+  {
+    this.confirmationService.confirm
+    (
+      {
+        message: 'Are you sure you want to delete this record?',
+        header: 'Confirmation',
+        icon: 'pi pi-exclamation-triangle',
+        accept: () =>
+        {
+          this.userService.deleteUser(userId).subscribe
+          (
+            {
+              next: (res) =>
+              {
+                console.log('User deleted successfully');
+                this.users = this.users.filter(user => user._id != userId);
+                this.messageService.add({severity: 'success', summary: 'Success', detail: 'User deleted successfully'});
+              },
+              error: (e) =>
+              {
+                console.log(e)
+              }
+            }
+          )
+        },
+        reject: (type: any) =>
+        {
+          switch(type)
+          {
+            case ConfirmEventType.REJECT:
+              console.log('User rejected this operation');
+              break;
+            case ConfirmEventType.CANCEL:
+              console.log('User canceled this operation')
+              break
+          }
         }
-      },
-    });
+      }
+    )
   }
 }
